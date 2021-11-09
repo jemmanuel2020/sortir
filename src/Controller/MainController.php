11e68dfa464e6539a277197d5a0018d737d93dd8
Julie-Lydie.Emmanuel <Julie-Lydie.Emmanuel@PORT_F77L373>
2021-11-09 16:50:02 +0100

    installation

---
+++ src/Controller/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #une methode par page
+    /**
+     * @Route("/", name="main_home")
+     */
+    public function home()
+    {
+        echo "coucou";
+        die();
+    }
+}
