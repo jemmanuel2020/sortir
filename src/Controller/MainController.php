2a6947b9087a30338f46d0723f0005ac4b6b888d
Julie-Lydie.Emmanuel <Julie-Lydie.Emmanuel@PORT_F77L373>
2021-11-10 10:23:02 +0100

    Mise en place du projet

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use http\Env\Response;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
