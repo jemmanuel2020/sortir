8795345f9176973a311ac24059e40dbe4a211201
Julie-Lydie.Emmanuel <Julie-Lydie.Emmanuel@PORT_F77L373>
2021-11-23 15:36:35 +0100

    Julie - Liste sortie

---
+++ src/Form/FiltreType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Participant;
+use App\Modele\Modele;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FiltreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomCampus', EntityType::class, [
+                'class' => Campus::class,
+                'choice_label' => 'nom',
+                'label' => 'Campus'
+            ])
+            ->add('nomSortie', SearchType::class, [
+                'label' => 'Le nom de la sortie contient:'
+            ])
+            ->add('dateSortie1', DateType::class, [
+                'label' => 'Entre',
+                'html5' => true,
+                'widget' => 'single_text'
+            ])
+            ->add('dateSortie2', DateType::class, [
+                'label' => 'et',
+                'html5' => true,
+                'widget' => 'single_text'
+            ])
+            ->add('organisateur', CheckboxType::class, [
+                'label' => 'Sorties dont je suis l\'organisateur/trice'
+            ])
+            ->add('inscrit', CheckboxType::class, [
+                'label' => 'DSorties auxquelles je suis inscrit/e'
+            ])
+            ->add('pasInscrit', CheckboxType::class, [
+                'label' => 'Sorties auxquelles je ne suis pas inscrit/e'
+            ])
+            //Ville
+            ->add('sortiepassees', CheckboxType::class, [
+                'label' => 'Sorties passées'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Modele::class,
+        ]);
+    }
+}
---
+++ src/Modele/Modele.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Modele;
+
+use App\Repository\ModeleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ModeleRepository::class)
+ */
+class Modele
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nomCampus;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nomSortie;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateSortie1;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateSortie2;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $organisateur;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $inscrit;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $pasInscrit;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $sortiePassees;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomCampus(): ?string
+    {
+        return $this->nomCampus;
+    }
+
+    public function setNomCampus(?string $nomCampus): self
+    {
+        $this->nomCampus = $nomCampus;
+
+        return $this;
+    }
+
+    public function getNomSortie(): ?string
+    {
+        return $this->nomSortie;
+    }
+
+    public function setNomSortie(?string $nomSortie): self
+    {
+        $this->nomSortie = $nomSortie;
+
+        return $this;
+    }
+
+    public function getDateSortie1(): ?\DateTimeInterface
+    {
+        return $this->dateSortie1;
+    }
+
+    public function setDateSortie1(?\DateTimeInterface $dateSortie1): self
+    {
+        $this->dateSortie1 = $dateSortie1;
+
+        return $this;
+    }
+
+    public function getDateSortie2(): ?\DateTimeInterface
+    {
+        return $this->dateSortie2;
+    }
+
+    public function setDateSortie2(?\DateTimeInterface $dateSorite2): self
+    {
+        $this->dateSortie2 = $dateSorite2;
+
+        return $this;
+    }
+
+    public function getOrganisateur(): ?bool
+    {
+        return $this->organisateur;
+    }
+
+    public function setOrganisateur(?bool $organisateur): self
+    {
+        $this->organisateur = $organisateur;
+
+        return $this;
+    }
+
+    public function getInscrit(): ?bool
+    {
+        return $this->inscrit;
+    }
+
+    public function setInscrit(?bool $inscrit): self
+    {
+        $this->inscrit = $inscrit;
+
+        return $this;
+    }
+
+    public function getPasInscrit(): ?bool
+    {
+        return $this->pasInscrit;
+    }
+
+    public function setPasInscrit(?bool $pasInscrit): self
+    {
+        $this->pasInscrit = $pasInscrit;
+
+        return $this;
+    }
+
+    public function getSortiePassees(): ?bool
+    {
+        return $this->sortiePassees;
+    }
+
+    public function setSortiePassees(?bool $sortiePassees): self
+    {
+        $this->sortiePassees = $sortiePassees;
+
+        return $this;
+    }
+}
