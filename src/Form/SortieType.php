d1c39aa335029ffdad1e00ce428db16533059e62
tlamboul2020 <thomas.lamboul2020@campus-eni.fr>
2021-11-26 09:29:07 +0100

    correction bug des états des sorties après création/modification + vue de la fonctionnalité annuler sortie

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -98,6 +99,12 @@ class SortieType extends AbstractType
                 'mapped' => false,
                 'disabled' => true
             ])
+            ->add('enregistrer', SubmitType::class, array(
+                'label' => 'Enregistrer'
+            ))
+            ->add('publier', SubmitType::class, array(
+                'label' => 'Publier'
+            ))
         ;
             $builder
                 ->addEventListener(FormEvents::PRE_SET_DATA, array($this, 'onPreSetData'))
